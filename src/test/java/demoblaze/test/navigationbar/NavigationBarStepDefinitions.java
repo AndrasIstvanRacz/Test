b8bbcaaf7d24e05bbb8026ec97b77a59c9e391f1
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2021-05-04 13:25:20 +0200

    Test 2

--- src/test/java/demoblaze/test/navigationbar/NavigationBarStepDefinitions.java
+++ src/test/java/demoblaze/test/navigationbar/NavigationBarStepDefinitions.java
@@ -1,6 +1,6 @@
 package demoblaze.test.navigationbar;
 
-import demoblaze.test.setup.Setup;
+import demoblaze.test.setup.Base;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
@@ -9,27 +9,26 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertEquals;
 
-public class NavigationBarStepDefinitions {
+public class NavigationBarStepDefinitions extends Base {
 
-    Setup setup = new Setup();
-    WebDriverWait wait = new WebDriverWait(setup.driver,40);
+    WebDriverWait wait = new WebDriverWait(driver,40);
 
 
     @Given("the user press the {string} button")
     public void buttonPressed(String btnXpath) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(btnXpath)));
-        setup.driver.findElement(By.xpath(btnXpath)).click();
+        driver.findElement(By.xpath(btnXpath)).click();
     }
 
     @Then("the {string} page should be loaded")
     public void testLinkLoaded(String link) {
-        assertEquals(setup.driver.getCurrentUrl(), link);
+        assertEquals(driver.getCurrentUrl(), link);
     }
 
     @Then("the {string} should showed")
     public void testContact(String popupXpath) throws InterruptedException {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(popupXpath)));
-        assertEquals(setup.driver.findElement(By.xpath(popupXpath)).getAttribute("class"), "modal fade show");
+        assertEquals(driver.findElement(By.xpath(popupXpath)).getAttribute("class"), "modal fade show");
     }
 
 }
